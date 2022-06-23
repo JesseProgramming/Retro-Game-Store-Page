d9ec937087e9734666fee28e3e8e282a63fc7a48
Jesse <101078250+JessePython@users.noreply.github.com>
2022-06-22 18:59:21 -0700

    Resolved debugger conflicts

---
+++ src/pages/home.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import '../styles/home.scss';
+function Home() {
+    return(
+        <div className="main-container">
+            <nav>
+                <a>The Retro Game Store</a>
+                <ul>
+                    <li>Cart</li>
+                    <li>Checkout</li>
+                </ul>
+                
+            </nav>
+        </div>
+    );
+}
+
+
+export default Home;
