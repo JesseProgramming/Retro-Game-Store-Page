f0752c1b6f8a92f5e1d74f434deba2d7d0c74d78
Jesse <101078250+JessePython@users.noreply.github.com>
2022-06-22 19:26:00 -0700

    Fixed json loading issue

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,15 +1,20 @@
 import React from 'react';
 import '../styles/home.scss';
+//import productData from '../data/products.json';
+const json = require('../data/products.json');
+console.log(json);
+
+
 function Home() {
     return(
-        <div className="main-container">
+        <div className="main-container"> 
             <nav>
                 <a>The Retro Game Store</a>
                 <ul>
                     <li>Cart</li>
                     <li>Checkout</li>
                 </ul>
-                
+
             </nav>
         </div>
     );
--- webpack.config.js
+++ webpack.config.js
@@ -57,7 +57,7 @@ module.exports = {
               {
                 test: /\.html$/i,
                 loader: "html-loader",
-              },
+              }
               
         ]
     },
